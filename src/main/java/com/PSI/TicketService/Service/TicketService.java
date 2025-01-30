be0945de691e5ee6ef68b9d68f9e27b3f1a8e9ce
Pawel K <pkudlacik@proton.me>
2025-01-30 17:21:21 +0100

    ticket service - returns dtos

--- src/main/java/com/PSI/TicketService/Service/TicketService.java
+++ src/main/java/com/PSI/TicketService/Service/TicketService.java
@@ -1,4 +1,43 @@
 package com.PSI.TicketService.Service;
 
+import com.PSI.TicketService.DTOs.TicketDTO;
+import com.PSI.TicketService.Enums.TicketReservationState;
+import com.PSI.TicketService.Mappers.TicketMapper;
+import com.PSI.TicketService.Models.TicketEntity;
+import com.PSI.TicketService.Repository.TicketRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class TicketService {
+
+    private final TicketRepository ticketRepository;
+
+    public TicketService(TicketRepository ticketRepository){
+        this.ticketRepository = ticketRepository;
+
+    }
+
+    public List<TicketDTO> getTicketsForEvent(Long eventId) {
+        List<TicketEntity> tickets = ticketRepository.findAllByEventId(eventId);
+        List<TicketDTO> ticketDTOs = tickets.stream()
+                .map(TicketMapper::MapTicketEntityToTicketDTO)
+                .collect(Collectors.toList());
+
+        return ticketDTOs;
+    }
+
+    public List<TicketDTO> getUnreservedTicketsForEvent(Long eventId) {
+        List<TicketEntity> tickets = ticketRepository.findAllByEventIdAndTicketState(eventId, TicketReservationState.UNRESERVED);
+
+        List<TicketDTO> ticketDTOs = tickets.stream()
+                .map(TicketMapper::MapTicketEntityToTicketDTO)
+                .collect(Collectors.toList());
+
+        return ticketDTOs;
+    }
+
 }
