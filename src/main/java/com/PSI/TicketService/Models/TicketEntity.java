ea7d84c9decab46640da405062298e815d0bfd0c
Pawel K <pkudlacik@proton.me>
2025-01-30 16:45:29 +0100

    ticket model changed, Long not long for null values, enums as ints

--- src/main/java/com/PSI/TicketService/Models/TicketEntity.java
+++ src/main/java/com/PSI/TicketService/Models/TicketEntity.java
@@ -1,4 +1,43 @@
 package com.PSI.TicketService.Models;
 
+
+import com.PSI.TicketService.Enums.TicketReservationState;
+import com.PSI.TicketService.Enums.TicketValidity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class TicketEntity {
+    @Id
+    private long id;
+
+    @Column(nullable=false)
+    private long seatId;
+
+    @Column(nullable=true)
+    private Long orderId;
+
+    @Column(nullable=false)
+    private long eventId;
+
+    @Column(nullable=false)
+    private long sectionId;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable=false)
+    private TicketValidity validity;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable=false)
+    private TicketReservationState ticketState;
+
+    // idk, było w modelu informacyjnym
+    //private int number;
+
+    @Column(nullable=false)
+    private double price;
+
+    private String qrCode;
+
 }
