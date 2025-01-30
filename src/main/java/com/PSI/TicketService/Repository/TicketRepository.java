7b8c6f5bfd928bf3693fcbe8ce5f07749caf9515
Pawel K <pkudlacik@proton.me>
2025-01-30 17:23:08 +0100

    ticket repo search by enum not int

--- src/main/java/com/PSI/TicketService/Repository/TicketRepository.java
+++ src/main/java/com/PSI/TicketService/Repository/TicketRepository.java
@@ -1,7 +1,14 @@
 package com.PSI.TicketService.Repository;
 
+import com.PSI.TicketService.Enums.TicketReservationState;
 import com.PSI.TicketService.Models.TicketEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TicketRepository extends JpaRepository<TicketEntity, Long> {
+    List<TicketEntity> findAllByEventId(Long eventId);
+    List<TicketEntity> findAllByEventIdAndTicketState(Long eventId, TicketReservationState ticketReservationState);
 }
