7d994c1fb804cdd6bab232c6b9879e8a11a873e6
Pawel K <pkudlacik@proton.me>
2025-01-30 17:20:32 +0100

    price type set to double

--- src/main/java/com/PSI/TicketService/DTOs/TicketDTO.java
+++ src/main/java/com/PSI/TicketService/DTOs/TicketDTO.java
@@ -1,10 +1,12 @@
 package com.PSI.TicketService.DTOs;
 
 import com.PSI.TicketService.Enums.TicketReservationState;
+import lombok.Data;
 
+@Data
 public class TicketDTO {
     private long id;
     private TicketReservationState reservationState;
     private long seatId;
-    private String price;
+    private double price;
 }
