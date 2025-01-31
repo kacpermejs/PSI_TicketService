f3fb53aa18a0070ec826f182b7383a7f83a17570
Pawel K <pkudlacik@proton.me>
2025-01-31 14:43:35 +0100

    data sql changed, seatId and sectionId are not schematicIds

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -2,29 +2,44 @@
 DELETE FROM ticket_entity;
 
 -- ENUM validity
-    -- CHECKED = 0
+    -- INVALID = 0
     -- VALID = 1
-    -- INVALID = 2
+    -- CHECKED = 2
 
 -- ENUM ticketState
-    -- RESERVED = 0
-    -- UNRESERVED = 1
+    -- UNRESERVED = 0
+    -- RESERVED = 1
     -- PAID = 2
 
--- section_id = schematic_object_id, przynajmniej ja tak to rozumiem
-INSERT INTO ticket_entity (id, seat_id, order_id, event_id, section_id, validity, ticket_state, price, qr_code) VALUES
-(1,  1, 1,    1, 1, 1, 0, 21.37, 'abcdef'),
-(2,  2, 1,    1, 1, 1, 0, 21.37, 'bcdefa'),
-(3,  3, 1,    1, 1, 1, 0, 21.37, 'cdefab'),
-(4,  4, 1,    1, 1, 1, 0, 21.37, 'defabc'),
+-- section_id, row_id, seat_id = schematic_object_id, przynajmniej ja tak to rozumiem i inaczej pierdole numeracje na tickecie
+--INSERT INTO ticket_entity (id, seat_id, order_id, event_id, section_id, validity, ticket_state, price, qr_code) VALUES
+--(1,  1, 1,    1, 1, 1, 0, 21.37, 'abcdef'),
+--(2,  2, 1,    1, 1, 1, 0, 21.37, 'bcdefa'),
+--(3,  3, 1,    1, 1, 1, 0, 21.37, 'cdefab'),
+--(4,  4, 1,    1, 1, 1, 0, 21.37, 'defabc'),
 -- bez przydziału do jakiegos order
-(5,  5, NULL, 1, 8, 1, 1, 21.37, 'efabcd'),
-(6,  6, NULL, 1, 8, 1, 1, 21.37, 'fabcde'),
+--(5,  5, NULL, 1, 8, 1, 0, 21.37, 'efabcd'),
+--(6,  6, NULL, 1, 8, 1, 0, 21.37, 'fabcde'),
 -- inny event
-(7,  1, 2,    2, 1, 0, 2, 21.37, 'xxxxxx'),
-(8,  2, 2,    2, 1, 0, 2, 21.37, 'yyyyyy'),
-(9,  3, 2,    2, 1, 0, 2, 21.37, 'zzzzzz'),
-(10, 4, 2,    2, 1, 0, 2, 21.37, 'wwwwww')
+--(7,  1, 2,    2, 1, 2, 2, 21.37, 'xxxxxx'),
+--(8,  2, 2,    2, 1, 2, 2, 21.37, 'yyyyyy'),
+--(9,  3, 2,    2, 1, 2, 2, 21.37, 'zzzzzz'),
+--(10, 4, 2,    2, 1, 2, 2, 21.37, 'wwwwww')
+--;
+
+-- section_id, row_id, seat_id = schematic_object_id, przynajmniej ja tak to rozumiem i inaczej pierdole numeracje na tickecie
+-- jednak nie schematic id
+INSERT INTO ticket_entity (id, order_id, event_id, seat_id, section_id, validity, ticket_state, price, qr_code) VALUES
+(1,  1, 1,    1, 1, 1, 1, 21.37, 'abcdef'),
+(2,  1, 1,    2, 1, 1, 1, 21.37, 'bcdefa'),
+(3,  1, 1,    3, 1, 1, 1, 21.37, 'cdefab'),
+ (4, 1, 1,    4, 1, 1, 1, 21.37, 'defabc'),
+-- bez przydziału do jakiegos order
+(5,  NULL, 1, 5, 2, 1, 0, 21.37, 'efabcd'),
+(6,  NULL, 1, 6, 2, 1, 0, 21.37, 'fabcde'),
+-- inny event i zakupione
+(7,  2, 2,    1, 1, 2, 2, 21.37, 'xxxxxx'),
+(8,  2, 2,    2, 1, 2, 2, 21.37, 'yyyyyy'),
+(9,  2, 2,    3, 1, 2, 2, 21.37, 'zzzzzz'),
+(10, 2, 2,    4, 1, 2, 2, 21.37, 'wwwwww')
 ;
-
-
